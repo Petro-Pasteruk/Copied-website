2a56f596f36494fc025e5b867dcd7b27830058b2
Petro Pasteruk <petropasteruk@gmail.com>
2024-08-26 19:15:04 +0300

    Add deep link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,11 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
                 <img height="1" width="1" style={{display: "none"}}
                      src="https://www.facebook.com/tr?id=7722995224406373&ev=PageView&noscript=1"
                 /></noscript>
+            <meta property="al:ios:url" content="nextboard://" />
+            <meta property="al:ios:app_store_id" content="1517022963" />
+            <meta property="al:ios:app_name" content="Copied" />
             <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+            <link rel="icon" href="/favicon/favicon.ico"/>
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
             <link rel="manifest" href="/favicon/site.webmanifest"/>
